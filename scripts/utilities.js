38e4f04dcdf50bdc831d3120d04871fa69b9186e
Arka Raina <arkaraina1@gmail.com>
2017-03-13 13:09:26 +0000

    Assignment 23 dom scripting events: complete

---
+++ scripts/utilities.js
@@ -0,0 +1,19 @@
+var pointsArray = document.getElementsByClassName('point');
+    
+for (var i = 0; i < pointsArray.length; i++) {
+    return function forEach();
+}
+
+function forEach(points[index].style, callback) {
+    points[index].style.opacity = 1;
+    points[index].style.transform = "scaleX(1) translateY(0)";
+    points[index].style.msTransform = "scaleX(1) translateY(0)";
+    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+    callback();
+}
+
+function myCallback() {
+    
+}
+
+forEach(scaleX(1) translateY(0), myCallback);
