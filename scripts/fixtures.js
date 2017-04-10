628bcef1a2981c633a543e3d74e9dbe82219e515
Arka Raina <arkaraina1@gmail.com>
2017-04-10 18:43:16 +0100

    Checkpoint 32 jquery: complete

---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+ 
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+ var albumTheBeatles = {
+     title: 'A Hard Day Night',
+     artist: 'The Beatles',
+     label: 'Parlophone',
+     year: '1964',
+     albumArtUrl: 'assets/images/album_covers/15.png',
+     songs: [
+         { title: 'Hey Jude?', duration: '4:34' },
+         { title: 'Yellow Submarine', duration: '5:09' },
+         { title: 'Eleanor Rigby', duration: '2:20'},
+         { title: 'If I Fell', duration: '4:41' },
+         { title: 'When I Get Home', duration: '2:39'}
+     ]
+ };
